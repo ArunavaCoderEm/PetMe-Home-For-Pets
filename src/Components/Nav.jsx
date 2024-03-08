28bd721f0348c4bb39c8a598308ea085a140c379
Arunava Dutta <acecankill@gmail.com>
2024-03-08 21:34:03 +0530

    final push of today

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -70,16 +70,6 @@ function Nav() {
                 <ion-icon name="chatbubbles-outline"></ion-icon>
               </div>
               <span className="link-text">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="tomato" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>
-                .About.</span>
-            </a>
-          </li>
-          <li className="navbar-item flexbox-left">
-            <a className="navbar-item-inner flexbox-left">
-              <div className="navbar-item-inner-icon-wrapper flexbox">
-                <ion-icon name="chatbubbles-outline"></ion-icon>
-              </div>
-              <span className="link-text">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="tomato" d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm80 256h64c44.2 0 80 35.8 80 80c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16c0-44.2 35.8-80 80-80zm-32-96a64 64 0 1 1 128 0 64 64 0 1 1 -128 0zm256-32H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/></svg>
                 .Contact.</span>
             </a>

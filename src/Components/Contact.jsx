38fb71ae587e72eb1e73bafafbd269b757390a61
ArunavaCoderEm <acecankill@gmail.com>
2024-03-25 00:24:25 +0530

    again

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,7 +4,9 @@ import "../CSS/Contact.css"
 export default function () {
   return (
      <>
-  
+      <div className='container justify-content-center text-center'>
+        <h3 className='text-white justify-content-center'>Contact Us</h3>
+      </div>
      </>
   )
 }

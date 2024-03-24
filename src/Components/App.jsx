8533963b2e1e1b069799b3aa308c585885b34f02
ArunavaCoderEm <acecankill@gmail.com>
2024-03-25 01:11:26 +0530

    last and tired

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,6 +3,7 @@ import Nav from '../Components/Nav'
 import Contact from '../Components/Contact'
 import Search from '../Components/Search'
 import Caraousal from '../Components/Caraousal'
+import Sign from '../Components/Sign'
 import  '../CSS/index.css'
 import { Route,Routes } from 'react-router-dom';
 
@@ -26,6 +27,7 @@ function App() {
       <Routes>  
         <Route  path="/"  element={<Caraousal/>}></Route>      
         <Route path="/contact"  element={<Contact/>} />
+        <Route path="/signin"  element={<Sign/>} />
       </Routes>
     </>
   )

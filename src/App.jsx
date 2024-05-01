ae9fe932af8ad16eb5136a0cd23b9284ba719726
ArunavaCoderEm <acecankill@gmail.com>
2024-05-01 23:23:56 +0530

    a lot of changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import Nav from './Components/Nav'
 import React from 'react'
-import Pages from './Pages/Pages'
 import './CSS/index.css'
 
 function App() {
   return (
     <>
-      <Nav/>
-      <Pages/>
+
     </>
   )
 }
--- src/Pages/Sign.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function () {
-  return (
-    <div>
-        <h1 className='mt-24 text-white text-center font-bold text-4xl'>Sign In</h1>
-    </div>
-  )
-}

4fdb9d1fc39aacfacd7a95effca22052fd2eb58e
ArunavaCoderEm <acecankill@gmail.com>
2024-05-04 20:36:06 +0530

    push fixes

--- firebase.js
+++ firebase.js
@@ -30,6 +30,6 @@ const firebaseConfig = {
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const auth = getAuth(app);
 
-export { app, auth }
+export default app

17c8cd4ec08ffcf1f8e9f720ec48064b03093b11
ArunavaCoderEm <acecankill@gmail.com>
2024-05-04 19:06:51 +0530

    a lot of pushes

--- src/Pages/Page.jsx
+++ src/Pages/Page.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from '../Components/Nav'
+import { Route, Routes } from 'react-router-dom'
+import Signup from './Signup'
+import Login from './Login'
 
 export default function Page() {
   return (
     <>
     <Navbar/>
+    <Routes>
+      <Route path='/signin' element={<Signup/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+    </Routes>
     </>
   )
 }

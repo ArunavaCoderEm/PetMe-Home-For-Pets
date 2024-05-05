02e3935c722f39e0596907d5fb5167a6905278b6
ArunavaCoderEm <acecankill@gmail.com>
2024-05-05 21:01:09 +0530

    pushes and finalized fixes

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -17,12 +17,12 @@ export default function Navbar() {
   })  
 
   return (
-    <nav className={`${(width < 667) ? 'bg-blue-950' : 'bg-sky-600'} py-2`}>
+    <nav className={`${(width < 667) ? 'bg-blue-950' : 'bg-blue-950'} py-2`}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
             <Link to="/" onClick={() => setMenu('home')} className="flex">
               <img src='./logopet.png' alt='logo' className='w-12 h-auto' />
-              <h2 className='text-3xl p-1 font-bold text-white cursor-pointer'><span className='hover:text-sky-700 transition-all duration-200'>PetMe</span></h2>
+              <h2 className='text-3xl p-1 font-bold text-white cursor-pointer'><span className='hover:text-green-300 transition-all duration-200'>PetMe</span></h2>
             </Link>
           <div className="flex items-center m-auto">
             <div className="hidden md:block ml-auto">
@@ -43,7 +43,7 @@ export default function Navbar() {
                 </Link>
               <Link to='/signin' className="text-white bg-gradient-to-b from-blue-600 to-blue-900 px-3 py-[14px] rounded-md text-md font-medium flex">
               <svg xmlns="http://www.w3.org/2000/svg" className='w-4 mx-2' viewBox="0 0 448 512"><path fill="white" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
-                <span className='transition-all duration-200 hover:text-sky-300'>Sign-Up</span></Link>
+                <span className='transition-all duration-200 hover:text-green-300'>Sign-Up</span></Link>
             </div>
           </div>
           <div className="-mr-2 flex md:hidden">
@@ -66,8 +66,8 @@ export default function Navbar() {
           <Link to='/features' className={`text-white  block px-3 py-2 rounded-md text-base font-medium ${(menu === 'features' ? 'bg-gradient-to-b from-blue-600 to-blue-900' : '')}`} onClick={()=>setMenu('features')}><span className='hover:text-green-300'>Features</span></Link>
           <Link to='/contact' className={`text-white  block px-3 py-2 rounded-md text-base font-medium ${(menu === 'contact' ? 'bg-gradient-to-b from-blue-600 to-blue-900' : '')}`} onClick={()=>setMenu('contact')}><span className='hover:text-green-300'>Contact</span></Link>
         </div>
-        <div className="pt-4 pb-3">
-          <div className="flex items-center px-5">
+        <div className="pt-4 pb-3 m-auto">
+          <div className="flex items-center px-5 justify-center align-middle">
           <div className="ml-5 flex items-center md:ml-6" onClick={()=> setMenu("")}>
                 <Link to='/cart' className="flex mx-2 text-white bg-gradient-to-b from-blue-600 to-blue-900 px-2 rounded-md text-md font-medium hover:fill-sky-400">
                 <svg xmlns="http://www.w3.org/2000/svg" className='w-5 h-auto' viewBox="0 0 576 512"><path fill='white' d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
@@ -75,7 +75,7 @@ export default function Navbar() {
                 </Link>
               <Link to='/signin' className="text-white bg-gradient-to-b from-blue-600 to-blue-900 px-3 py-[14px] rounded-md text-md font-medium flex">
               <svg xmlns="http://www.w3.org/2000/svg" className='w-4 mx-2' viewBox="0 0 448 512"><path fill="white" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
-                <span className='transition-all duration-200 hover:text-sky-300'>Sign-Up</span></Link>
+                <span className='transition-all duration-200 hover:text-green-300'>Sign-Up</span></Link>
             </div>
             </div>
         </div>

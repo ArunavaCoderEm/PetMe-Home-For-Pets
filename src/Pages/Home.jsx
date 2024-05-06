7ce8738b4368f1162c90dac09dfb82561329ed9e
ArunavaCoderEm <acecankill@gmail.com>
2024-05-07 01:03:55 +0530

    ajker moto byass

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Caraousal from '../Components/Caraousal'
 import Hero from '../Components/Hero'
+import BelowHeroHome from '../Components/BelowHeroHome'
 
 export default function Home() {
   return (
     <>
       <Hero/>
       <Caraousal/>
+      <BelowHeroHome/>
     </>
   )
 }

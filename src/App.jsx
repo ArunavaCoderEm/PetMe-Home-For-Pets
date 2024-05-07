d5399f53a2f81ea132e6dee6c4afdf5cb15f557c
ArunavaCoderEm <acecankill@gmail.com>
2024-05-08 01:44:44 +0530

    loader done now will make it look good tomorrow

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CSS/index.css'
 import Page from './Page'
+import Loader from './Components/Loader'
 
 function App() {
+
+  const [loading,setloading] = useState(true);
+
+  setTimeout(() => {
+    setloading(false);
+  }, 2500);
+
   return (
     <>
-    <Page/>
+    {(loading)  ? 
+      <Loader/> :
+      <Page/>
+    }
     </>
   )
 }

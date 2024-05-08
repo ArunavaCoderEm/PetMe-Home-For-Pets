fc5a6242135aa0bf65be7dd86606cbee9a36ec9f
ArunavaCoderEm <acecankill@gmail.com>
2024-05-09 01:18:51 +0530

    I need to find some api else i'll find something sure and site will be kind of static and all

--- src/Components/BelowHeroHome.jsx
+++ src/Components/BelowHeroHome.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function BelowHeroHome() {
   return (
     <>
+      <div className='bg-transparent'>
+        <h1 className='text-3xl font-extrabold bg-clip-text text-center text-transparent bg-gradient-to-l from-blue-100 to-blue-400'>Most Loved Dogs</h1>
         
+      </div> 
     </>
   )
 }

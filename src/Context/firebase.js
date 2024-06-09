e36fcbd86c91de94f1d1f71fe79e452545fb20ec
ArunavaCoderEm <acecankill@gmail.com>
2024-06-09 22:21:41 +0530

    modification

--- src/Context/firebase.js
+++ src/Context/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZk_Cg0AXWq6a-QLvWUXTgZMp_5EYUT14",
-  authDomain: "petme-d7039.firebaseapp.com",
-  projectId: "petme-d7039",
-  storageBucket: "petme-d7039.appspot.com",
-  messagingSenderId: "656484110784",
-  appId: "1:656484110784:web:7fc1b95bd4b58954c6f825"
+  apiKey: import.meta.env.VITE_FB_API,
+  authDomain:import.meta.env.VITE_FB_AUT,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGE,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGE,
+  appId: import.meta.env.VITE_FB_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);

8d4d1d214ae759738496e373f987d5a5d3a7ee19
ArunavaCoderEm <acecankill@gmail.com>
2024-06-09 23:28:13 +0530

    cards done push

--- src/Components/BelowHeroHome.jsx
+++ src/Components/BelowHeroHome.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import Card from './Card'
 
 export default function BelowHeroHome() {
   return (
     <>
       <div className='bg-transparent'>
-        <h1 className='text-3xl font-extrabold bg-clip-text text-center text-transparent bg-gradient-to-l from-blue-100 to-blue-400'>Most Loved Dogs</h1>
-        
+        <h1 className='text-3xl font-extrabold bg-clip-text text-center text-transparent bg-gradient-to-l from-blue-100 to-blue-300'>Most Loved Dogs</h1>
+        <div className='my-5 mx-4 grid lg:grid-cols-3 sm:grid-cols-1'>
+          <Card/>
+        </div>
+        <h1 className='text-3xl font-extrabold bg-clip-text text-center text-transparent bg-gradient-to-l from-blue-100 to-blue-300'>Most Loved Cats</h1>
+        <div className='my-5 mx-4 grid lg:grid-cols-3 sm:grid-cols-1'>
+          <Card/>
+        </div>
       </div> 
     </>
   )

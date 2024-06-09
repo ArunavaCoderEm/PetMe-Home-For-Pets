f94f5444e09d8465d36e591b5199b7df58be3f70
ArunavaCoderEm <acecankill@gmail.com>
2024-06-09 20:19:48 +0530

    push

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Loader() {
   return (
     <>
-    <div class='w-screen bg-gradient-to-t from-blue-700 via-blue-400 to-blue-700 z-50 overflow-hidden h-screen fixed'>
-    <div class="bg-transparent w-80 h-80 absolute top-0 right-0 left-0 bottom-0 m-auto mb-6">
-        <img src='./paw.png' class='paw w-20 m-auto absolute top-0 bottom-0 left-0 right-0 mb-8' />
-        <h2 class='w-full font-semibold text-white text-2xl p-5 text-center'>Pet a pet : A simple profound joy.</h2>
+    <div className='w-screen bg-gradient-to-t from-blue-700 via-blue-400 to-blue-700 z-50 overflow-hidden h-screen fixed'>
+    <div className="bg-transparent w-80 h-80 absolute top-0 right-0 left-0 bottom-0 m-auto mb-6">
+        <img src='./paw.png' className='paw w-20 m-auto absolute top-0 bottom-0 left-0 right-0 mb-8' />
+        <h2 className='w-full font-semibold text-white text-2xl p-5 text-center'>Pet a pet : A simple profound joy.</h2>
     </div>
     </div>
     </>
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -81,10 +81,10 @@ export default function Navbar() {
                   :
 
                   <>
-                   <h1 className="flex mx-2 text-white bg-gradient-to-b from-blue-600 to-blue-900 px-2 rounded-md text-md font-medium hover:fill-sky-400">
+                   <div className="flex mx-2 text-white bg-gradient-to-b from-blue-600 to-blue-900 px-2 rounded-md text-md font-medium hover:fill-sky-400">
                    <svg xmlns="http://www.w3.org/2000/svg" className='w-4 mx-2' viewBox="0 0 448 512"><path fill="white" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
-                      <h1 className='text-white p-3 hover:text-red-400'><span className='transition-all duration-200 hover:text-green-300'>{name}</span></h1>
-                  </h1>
+                      <h2 className='text-white p-3 hover:text-red-400'><span className='transition-all duration-200 hover:text-green-300'>{name}</span></h2>
+                  </div>
 
                    <Link to='/cart' className="flex mx-2 text-white bg-gradient-to-b from-blue-600 to-blue-900 px-2 rounded-md text-md font-medium hover:fill-sky-400">
                   <svg xmlns="http://www.w3.org/2000/svg" className='w-5 h-auto' viewBox="0 0 576 512"><path fill='white' d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>

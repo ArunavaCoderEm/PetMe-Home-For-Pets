02527f207fe57dbec8f1e0fa75687a0ed9e16b62
ArunavaCoderEm <acecankill@gmail.com>
2024-08-13 21:37:06 +0530

    puah change

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Footer() {
   return (
     <div className='bg-blue-800 bottom-0 relative left-0 mt-10'>
-        <h1 className='text-white text-center p-2'>Made by <Link to='https://godard.vercel.app/' target='_blank' className='mx-1 text-blue-300'>Godard</Link> with - Vite+React, Tailwind & 💜 </h1>
+        <h1 className='text-white text-center p-2'>Made by <Link to='https://meard.vercel.app/' target='_blank' className='mx-1 text-blue-300'>Godard</Link> with - Vite+React, Tailwind & 💜 </h1>
     </div>
   )
 }
